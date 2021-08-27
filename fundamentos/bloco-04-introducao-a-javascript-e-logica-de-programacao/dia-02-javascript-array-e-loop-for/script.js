62ace6d9b18e41abedf46694e2ecdaf69f075dbe
neguinho <victor_maanoel@Outlook.com>
2021-08-27 12:29:12 -0400

    salvando alterações

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -0,0 +1,58 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// ex-1
+// console.log(numbers)
+
+// ex-2
+//  var soma =0;
+//  for(let index =0; index < numbers.length; index += 1{
+//      soma +=  numbers[index];
+//      console.log(soma)
+// }
+
+//ex-3 
+// var soma =0;
+//  for(let index =0; index < numbers.length; index += 1){
+//     soma +=  numbers[index];
+//     let div = soma / numbers.length
+//      console.log(div)
+//}
+
+//ex-4
+// var soma = 0
+// for(let index =0; index < numbers.length; index += 1){
+//     soma +=  numbers[index];
+//     let div = soma / numbers.length
+//   console.log(div);
+
+//       if(div > 20) {
+//           console.log("valor maior que 20")
+//       } else {
+//          console.log("Valor menor ou igual a 20")
+//       }
+// }
+
+//ex -5
+
+// for(let index = 0; index < numbers.length; index +=1){
+//     let maiorIndex = numbers.sort()
+//     console.log(maiorIndex[0])
+// }
+
+//ex-6 incomplto
+
+// for(let index = 0; index < numbers.length; index +=1){
+//     let maiorIndex = numbers.sort()
+//     console.log(maiorIndex[])
+// }
+
+//ex-7
+// var soma = 0;
+// for(let index = 0; index < numbers.length; index +=1){
+//         soma += index
+//     if(soma % 2 === 1){
+//         console.log(`${soma} valor impar`)
+//     }else {
+//         console.log("nenhum valor impar encontrado")
+//     }
+// }
