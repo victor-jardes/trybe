40d59cfc848bad8f620b525f650e34a8e7d204cf
neguinho <victor_maanoel@Outlook.com>
2021-10-27 19:59:26 -0400

    ex 1 feito e iniciando o 2

---
+++ front-end/Bloco-10-introducao-a-react/ex1-react/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+function App() {
+  const compromise = ['Trybe', 'Dormir tarde', 'tarefas domesticas']
+  return (
+    <>
+      <ul>
+        <li>{compromise.map((item) => Task(item))}</li>
+      </ul>    
+    </>
+  )
+}
+
+export default App;
